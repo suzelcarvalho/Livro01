c64ccb2addaf21171eb87230c4ecdb89a54a8226
suzelcarvalho <suzelbrito@gmail.com>
2021-09-19 17:37:56 +0000

    arquivo java

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,8 +1,10 @@
 package application;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application{
     public static void main(String args[])
        
-
+       SpringApplication.run(Application.class,args);
+    } 
    
 }
