638dfda0311cfaa81980f4db9577c1d27e057459
suzelcarvalho <suzelbrito@gmail.com>
2021-09-19 17:51:28 +0000

    arquivo java

---
+++ src/main/java/application/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package application.controllers;
+
+import org.springframework.stereotype.controllers;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@controllers
+public class HomeControllers {
+    @Requestmappping(method=RequetMethod.GET)
+
+
+}
