4bb5edd71036a188ea50cb1a201649291a638423
suzelcarvalho <suzelbrito@gmail.com>
2021-09-21 19:40:20 +0000

    correção do arquivo homecontroller

--- src/main/java/application/controllers/HomeController.java
+++ src/main/java/application/controllers/HomeController.java
@@ -1,12 +1,12 @@
 package application.controllers;
 
-import org.springframework.stereotype.Controllers;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controllers
-public class HomeControllers {
-    @Requestmappping(method=RequetMethod.GET)
+@Controller
+public class HomeController {
+    @RequestMappping(method=RequetMethod.GET)
     public String index(){
         return "home/index.jsp"
     }
