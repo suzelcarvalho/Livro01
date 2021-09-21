4a1f8292b6b2c6066f28ad6db60cd14009560c48
suzelcarvalho <suzelbrito@gmail.com>
2021-09-21 19:31:08 +0000

    correção do arquivo Application

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,6 +1,7 @@
 package application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
 public class Application{
     public static void main(String args[])
        
